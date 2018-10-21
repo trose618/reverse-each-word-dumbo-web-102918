a768ecd7d2ebc4c5059bbe134d45b53bf3f06c81
Terrance Rose Jr <trose618@gmail.com>
2018-10-21 22:04:44 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,8 +2,8 @@ require 'pry'
 
 def reverse_each_word(sentence)
   a_sentence = sentence.split(" ")
-  a_sentence = a_sentence.collect do |word|
-    word = word.reverse
+  a_sentence.collect do |word|
+    word.reverse
   end
   a_sentence.join(" ")
 end
