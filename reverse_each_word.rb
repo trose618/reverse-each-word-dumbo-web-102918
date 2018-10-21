a610171bf9a67fb918f5912d82571a1ed2ef336b
Terrance Rose Jr <trose618@gmail.com>
2018-10-21 21:56:37 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -4,9 +4,6 @@ def reverse_each_word(sentence)
   a_sentence = sentence.split(" ")
   a_sentence.collect do |word|
     word = word.reverse
-    #binding.pry
   end
-  #binding.pry
   a_sentence.join(" ")
-  
 end
